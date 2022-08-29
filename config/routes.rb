9896fc2c6c7406f916985c8d8f1ca59122e073c4
hayato0413 <ikehaya.soccer@icloud.com>
2022-08-30 06:33:25 +0900

    アカウント詳細ページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  resources :post, expect: [:index] 
-
-  get "/",         to: 'post#index'
+  resources :account,       only:   [:show]
+  resources :post,          expect: [:index] 
+  
+  get "/",                  to: 'post#index'
 end
