41620f19f0b771321aa0bd40efcc59caed5312e6
hayato0413 <ikehaya.soccer@icloud.com>
2022-08-31 07:49:22 +0900

    投稿編集ページの実装

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -13,7 +13,20 @@ class PostController < ApplicationController
   end
 
   def show 
-    @post = Post.find_by(id: params[:id])
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update 
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(id: @post.id)
+    else 
+      render :edit
+    end
   end
 
   def post_user 
