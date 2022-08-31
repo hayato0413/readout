14b041e476479f96328669645cabc09b556f64ca
hayato0413 <ikehaya.soccer@icloud.com>
2022-08-31 11:00:03 +0900

    投稿詳細画面でのいいね機能の実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
+
+  def favorite?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+
   has_many :post_category_relations
   has_many :categories, through: :post_category_relations
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :account,         only:   [:show]
-  resources :post,            expect: [:index] 
+
+  resources :post,            expect: [:index] do
+    resource :favorites,        only: [:create, :destroy]
+  end
   
   get "/",                    to: 'post#index'
   get "/post/user/:id",       to: 'post#post_user'
