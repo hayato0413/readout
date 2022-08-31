ab60fa2223946973dd62df72feca8336cb409c63
hayato0413 <ikehaya.soccer@icloud.com>
2022-08-31 10:39:54 +0900

    favoriteコントローラーとアソシエーションの追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,7 @@ class Post < ApplicationRecord
   has_many :post_category_relations
   has_many :categories, through: :post_category_relations
 
+  has_many :favorites, dependent: :destroy
+
   belongs_to :user
 end
