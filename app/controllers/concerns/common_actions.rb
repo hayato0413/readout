187e88693680f597b224ce687a788b45aecd4cf9
hayato0413 <ikehaya.soccer@icloud.com>
2022-09-02 11:02:22 +0900

    ゲストログイン機能の実装

--- app/controllers/concerns/common_actions.rb
+++ app/controllers/concerns/common_actions.rb
@@ -4,4 +4,16 @@ module CommonActions
   def set_categories
     @categories = Category.all
   end
+
+  def guest? 
+    if user_signed_in?
+      if current_user.email == "guest@example.com"
+        true
+      else 
+        false
+      end
+    else 
+      false
+    end
+  end
 end
